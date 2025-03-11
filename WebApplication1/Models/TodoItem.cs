a9cb3fef7c64758aba7e53c39f60991f7d0ce350
JiunShiang880109 <71368367+JiunShiang880109@users.noreply.github.com>
2025-03-11 15:25:39 +0800

    2025/03/11 first commit

--- WebApplication1/Models/TodoItem.cs
+++ WebApplication1/Models/TodoItem.cs
@@ -1,8 +1,16 @@
-namespace TodoApi.Models;
-
-public class TodoItem
-{
-    public long Id{get; set;}
-    public string? Name{get; set;}
-    public bool IsComplete{get; set;}
+namespace TodoApi.Models{
+    public class TodoItem
+    {
+        public long Id{get; set;}
+        public string? Name{get; set;}
+        public bool IsComplete{get; set;}
+        /*DTO可用來：
+            防止過度張貼。
+            隱藏用戶端不應該檢視的屬性。
+            省略一些屬性，以減少承載大小。
+            包含巢狀物件的壓平合併物件圖形。 壓平合併物件圖形對用戶端會更方便。
+        */
+        public string? Secret{get; set;}
+    }
 }
+
--- WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61777f0af17d3832334d4f52fde83460f143ab74")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
